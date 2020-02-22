ee95a75be335e83adeb178ccea4e773ffc08f112
Tanmay Thakur <thakurtanmay72@yahoo.com>
2020-02-22 19:33:23 +0530

    First 200 images of each category taken

---
+++ preprocessing.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 22 17:10:00 2020
+
+@author: Tanmay Thakur
+"""
+import pickle
+
+from os import listdir
+from utils import convert_image_to_array
+
+
+directory_root = "PlantVillage/"
+
+image_list, label_list = [], []
+
+print("[INFO] Loading images ...")
+root_dir = listdir(directory_root)
+
+for plant_folder in root_dir :
+    plant_disease_folder_list = listdir(f"{directory_root}/{plant_folder}")
+
+    for plant_disease_folder in plant_disease_folder_list:
+        print(f"[INFO] Processing {plant_disease_folder} ...")
+        plant_disease_image_list = listdir(f"{directory_root}/{plant_folder}/{plant_disease_folder}/")
+            
+        for single_plant_disease_image in plant_disease_image_list :
+            if single_plant_disease_image == ".DS_Store" :
+                plant_disease_image_list.remove(single_plant_disease_image)
+
+        for image in plant_disease_image_list[:200]:
+            image_directory = f"{directory_root}/{plant_folder}/{plant_disease_folder}/{image}"
+            if image_directory.endswith(".jpg") == True or image_directory.endswith(".JPG") == True:
+                image_list.append(convert_image_to_array(image_directory))
+                label_list.append(plant_disease_folder)
+                
+print("[INFO] Image loading completed")  
+
+data_dump = image_list, label_list
+
+pickle_out = open("dict.pickle","wb")
+pickle.dump(data_dump, pickle_out)
+pickle_out.close()
