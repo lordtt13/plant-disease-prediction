89a606cf37a3e663faf242014c107be96643e71e
Tanmay Thakur <thakurtanmay72@yahoo.com>
2020-02-22 19:56:36 +0530

    try, catch removed

--- utils.py
+++ utils.py
@@ -7,11 +7,10 @@ Created on Sat Feb 22 17:07:01 2020
 import cv2
 import numpy as np
 
-from tensorflow.keras.preprocessing import img_to_array
+from tensorflow.keras.preprocessing.image import img_to_array
 
 
 default_image_size = tuple((256, 256))
-
 
 def convert_image_to_array(image_dir):
     image = cv2.imread(image_dir)
